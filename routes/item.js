b2d01bfec3ee4e478ece01591066802318af9aa4
unknown <ihourquescos@gmail.com>
2024-10-19 23:35:54 -0300

    cambio package

--- routes/item.js
+++ routes/item.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const ItemController = require("../controllers/Item");
+const ItemController = require("../controllers/item");
 
 //prettier-ignore
 router.post("/", ItemController.createItem);
